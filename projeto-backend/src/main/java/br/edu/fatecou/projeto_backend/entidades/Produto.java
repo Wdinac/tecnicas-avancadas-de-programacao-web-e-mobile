60bf745f4fe55427a9c1f8853c8ace2f0f72e6f0
Welliton Inácio <159810533+Wdinac@users.noreply.github.com>
2025-11-17 17:29:24 +0000

    Aula do Dia 17/11/2025

---
+++ projeto-backend/src/main/java/br/edu/fatecou/projeto_backend/entidades/Produto.java
@@ -0,0 +1,16 @@
+package br.edu.fatecou.projeto_backend.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Produto {
+    @Id
+    private String uuid;
+
+    private String nome;
+    private Integer quantidade;
+    private Double preco;
+}
